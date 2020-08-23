3f49e7e82cef129ff13d1ca451d92b0d6ae6eae5
Kaidrick <cjt422@gmail.com>
2020-08-23 22:02:56 +0800

    added some localization for testing tweaked page layouts

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ $t('message') }}</p>
+    <div class="lang">
+      <el-select v-model="$i18n.locale">
+        <el-option v-for="(lang, index) in languages" :key="`lang${index}}`" :value="lang">
+          {{ lang }}
+        </el-option>
+      </el-select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloI18n',
+
+  data() {
+    return {
+      languages: ['en', 'fr', 'zh']
+    }
+  }
+}
+</script>
+
+<!--<i18n>-->
+<!--{-->
+<!--  "en": {-->
+<!--    "hello": "Hello i18n in SFC!"-->
+<!--  }-->
+<!--}-->
+<!--</i18n>-->
