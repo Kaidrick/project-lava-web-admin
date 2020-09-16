1187ea9d603048eacd68a29f16003fc54cf17337
Jiutian Cai <cjt422@gmail.com>
2020-09-16 21:12:33 +0800

    switch to module based file structure work in progress commit

---
+++ src/modules/system/configuration/components/ScriptInstallConfig.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="script-install-config-wrapper">
+
+  </div>
+</template>
+
+<script>
+export default {
+name: "ScriptInstallConfig"
+}
+</script>
+
+<style lang="scss" scoped>
+  .script-install-config-wrapper {
+    
+  }
+</style>
