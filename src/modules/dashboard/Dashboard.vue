e565747cf9c046203c6748f9c73deb0020921026
Kaidrick <cjt422@gmail.com>
2020-10-11 23:57:33 +0800

    added assets for dashboard components

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -6,20 +6,45 @@
                     <Telemetry></Telemetry>
                 </div>
             </el-col>
+            <el-col class="block" :span="16">
+                <div class="block-content">
+                    <PlayerList></PlayerList>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col class="block" :span="8">
+                <div class="block-content">
+                    <h2>Operation</h2>
+                    <div>Broadcast</div>
+                    <el-input type="textarea"></el-input>
+                    <div>Controls</div>
+                    <el-button>Restart</el-button>
+                    <el-button>Stop</el-button>
+                </div>
+            </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
     import Telemetry from "./components/Telemetry";
+    import PlayerList from "./components/PlayerList";
     export default {
         name: "Dashboard",
-        components: {Telemetry}
+        components: {PlayerList, Telemetry}
     }
 </script>
 
 <style lang="scss" scoped>
     @import "src/assets/style/color-scheme";
+
+    .el-row {
+        margin-bottom: 20px;
+        &:last-child {
+            margin-bottom: 0;
+        }
+    }
 
     .dashboard-wrapper {
         background-color: transparent;
---
+++ src/modules/dashboard/components/PlayerList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="player-list-wrapper">
+        <el-table>
+            <el-table-column label="Net ID"></el-table-column>
+            <el-table-column label="Player Name"></el-table-column>
+            <el-table-column label="UCID"></el-table-column>
+            <el-table-column label="Ping"></el-table-column>
+            <el-table-column label="Client Language"></el-table-column>
+            <el-table-column label=""></el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "PlayerList"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .player-list-wrapper {
+
+    }
+</style>
